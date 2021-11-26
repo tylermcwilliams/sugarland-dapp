7461131f545de7a3de026a873ba44ef3b2e2ca92
tylermcwilliams <tyler.ellis.mcwilliams@gmail.com>
2021-11-26 17:55:34 +0100

    views use composables

--- components/cards/LoginCard.vue
+++ components/cards/LoginCard.vue
@@ -2,31 +2,34 @@
   <div class="flying">
     <PopFromShadow class="w-52">
       <ButtonDefault
+        v-if="!active"
         placeholder="Connect Your Wallet"
         @clicked="showMintingModal"
-      >
-      </ButtonDefault
-    ></PopFromShadow>
+      />
+      <span v-if="active"> {{ account }}... </span>
+    </PopFromShadow>
   </div>
 </template>
 
 <script>
 import { defineComponent } from "@nuxtjs/composition-api";
+import { useWeb3 } from "@instadapp/vue-web3";
 import { useModal } from "~/composables/useModal";
 import LoginPopup from "~/components/modals/LoginPopup.vue";
 export default defineComponent({
   name: "LoginCard",
   setup() {
+    const { active, account } = useWeb3();
     const { showMintingModal } = useModal(LoginPopup);
 
-    return { showMintingModal };
+    return {
+      showMintingModal,
+      active,
+      account,
+    };
   },
 });
 </script>
-
-
-
-
 
 <style scoped>
 .flying {
